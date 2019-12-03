9c19477c8a55f6f760cf9633d7f5359a5e29a9e4
qyuson <56363395+qyuson@users.noreply.github.com>
2019-12-03 16:39:01 +0000

    Organised files and codes

--- AQK Website/Scripts/smoothscroll.js
+++ AQK Website/Scripts/smoothscroll.js
@@ -1,14 +1,12 @@
-jQuery(document).ready(function($) {
-  // Scroll to the desired section on click
-  // Make sure to add the `data-scroll` attribute to your `<a>`          tag.
-  // Example:
-  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
-  function scrollToSection(event) {
-    event.preventDefault();
-    var $section = $($(this).attr('href'));
-    $('html, body').animate({
-      scrollTop: $section.offset().top
-    }, 500);
+/*
+	Smooth scroll functionality for anchor links (animates the scroll
+	rather than a sudden jump in the page)
+*/
+$('.js-anchor-link').click(function(e){
+  e.preventDefault();
+  var target = $($(this).attr('href'));
+  if(target.length){
+    var scrollTo = target.offset().top;
+    $('body, html').animate({scrollTop: scrollTo+'px'}, 800);
   }
-  $('[data-scroll]').on('click', scrollToSection);
-}(jQuery));
+});
